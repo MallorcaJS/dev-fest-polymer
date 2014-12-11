5bd5ede7aee6d8efc3e7f645ae3cf83cfc92d26a
Kevin O'Hagan <kevinohagan@gmail.com>
2014-12-11 19:53:10 +0100

    foto pretyy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.addEventListener('click', function(e) {
-  var pages = document.querySelector('core-pages');
-  pages.selected = (pages.selected + 1) % pages.children.length;
-});
+// document.addEventListener('click', function(e) {
+//   var pages = document.querySelector('core-pages');
+//   pages.selected = (pages.selected + 1) % pages.children.length;
+// });
+//
